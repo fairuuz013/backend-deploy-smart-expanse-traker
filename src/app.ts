0ec482111f758e760d1beffa1af6e79f6a118b77
fairuuz <justcall1313@gmail.com>
2026-02-04 22:00:51 +0700

    for ready deploy

--- src/app.ts
+++ src/app.ts
@@ -1,19 +1,21 @@
 import express, { type Application, type Request, type Response } from "express";
-import path from 'path'; // Tambahkan import ini
+import path from 'path';
 import cors from 'cors';
-import authRoutes from './routes/auth.routes';
-import userRoutes from './routes/user.routes';
-import walletRoutes from './routes/wallet.routes';
-import categoryRoutes from './routes/category.routes';
-import transactionRoutes from "./routes/transaction.routes";
-import dashboardRoutes from "./routes/dashboard.routes";
-import budgetRoutes from './routes/budget.routes';         
-import notificationRoutes from './routes/notification.routes';
-import aiRoutes from './routes/ai.routes';
-import { ErrorHandler } from './middlewares/error.handler';
+
+import authRoutes from './routes/auth.routes.js';           
+import userRoutes from './routes/user.routes.js';          
+import walletRoutes from './routes/wallet.routes.js';      
+import categoryRoutes from './routes/category.routes.js'; 
+import transactionRoutes from "./routes/transaction.routes.js"; 
+import dashboardRoutes from "./routes/dashboard.routes.js";    
+import budgetRoutes from './routes/budget.routes.js';          
+import notificationRoutes from './routes/notification.routes.js';
+import aiRoutes from './routes/ai.routes.js';           
+
+import { ErrorHandler } from './middlewares/error.handler.js'; 
 import swaggerUi from 'swagger-ui-express';
-import { swaggerSpec } from './utils/swagger.config';
-import { RateLimitMiddleware } from './middlewares/rateLimiter.middlerware'; 
+import { swaggerSpec } from './utils/swagger.config.js';       
+import { RateLimitMiddleware } from './middlewares/rateLimiter.middlerware.js';
 
 const app: Application = express();
 
@@ -22,12 +24,12 @@ app.set('trust proxy', 1);
 app.use(express.json());
 app.use(cors()); 
 
+
 app.use('/uploads', express.static(path.join(process.cwd(), 'uploads')));
 
 app.use(RateLimitMiddleware.globalLimiter);
 app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerSpec));
 
-// === ROUTES ===
 app.use('/api/auth', authRoutes);
 app.use('/api/users', userRoutes);    
 app.use('/api/wallets', walletRoutes); 
